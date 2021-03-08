b2f90830f64e70bdbbe05205d091796de3619508
Sandy Corsillo <the_dusky@hey.com>
2021-03-08 06:37:54 -0500

    updated testnet.js to match enigma

--- src/minting.test.js
+++ src/minting.test.js
@@ -4,5 +4,5 @@ jest.mock('axios');
 
 test('Check for minting inflation', async () => {
   const inflation = await getInflation();
-  expect(inflation.result).toBe('0.150000000000000001');
+  expect(inflation.result).toBe('0.15000000000000001');
 });
