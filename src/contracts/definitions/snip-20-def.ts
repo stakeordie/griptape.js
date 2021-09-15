bc93c27767b6378b930365fcfe1b755b769b3cb7
jsandovalg <jorge@stakeordie.com>
2021-09-15 15:37:08 -0700

    feat: Complete SNIP-20 contract definition (#28)

--- src/contracts/definitions/snip-20-def.ts
+++ src/contracts/definitions/snip-20-def.ts
@@ -8,6 +8,7 @@ import {
 
 export const snip20Def: ContractDefinition = {
   queries: {
+    //Base
     getBalance({ address, key }: Context): ContractRequest {
       return { balance: { address, key } };
     },
@@ -24,12 +25,24 @@ export const snip20Def: ContractDefinition = {
       return { transfer_history: { address, key, page_size, page } };
     },
 
+    //Mintable
+    getMinters(): ContractRequest {
+      return { minters: {} };
+    },
+
+    //Alowance
+    getAllowance(owner: string, spender: string, key: string): ContractRequest {
+      return { allowance: { owner, spender, key } };
+    },
+
+    //Native
     getExchangeRate(): ContractRequest {
       return { exchange_rate: {} };
     },
   },
 
   messages: {
+    //Base
     transfer({ padding }: Context, recipient: string, amount: string) {
       const handleMsg = {
         transfer: { recipient, amount, padding },
@@ -70,6 +83,86 @@ export const snip20Def: ContractDefinition = {
       return { handleMsg };
     },
 
+    //Alowances
+    increaseAllowances(
+      { padding }: Context,
+      spender: string,
+      amount: string,
+      expiration?: number
+    ) {
+      const handleMsg = {
+        increase_allowance: { spender, amount, expiration, padding },
+      };
+      return { handleMsg };
+    },
+
+    decreaseAllowance(
+      { padding }: Context,
+      spender: string,
+      amount: string,
+      expiration?: number
+    ) {
+      const handleMsg = {
+        decrease_allowance: { spender, amount, expiration, padding },
+      };
+      return { handleMsg };
+    },
+
+    transferFrom(
+      { padding }: Context,
+      owner: string,
+      recipient: string,
+      amount: string
+    ) {
+      const handleMsg = {
+        transfer_from: { owner, recipient, amount, padding },
+      };
+      return { handleMsg };
+    },
+
+    sendFrom(
+      { padding }: Context,
+      owner: string,
+      recipient: string,
+      amount: string,
+      msg?: string
+    ) {
+      const handleMsg = {
+        send_from: { owner, recipient, amount, msg, padding },
+      };
+      return { handleMsg };
+    },
+
+    //Mintable
+    mint({ padding }: Context, recipient: string, amount: string) {
+      const handleMsg = {
+        mint: { recipient, amount, padding },
+      };
+      return { handleMsg };
+    },
+
+    setMinters({ padding }: Context, minters: string[]) {
+      const handleMsg = {
+        set_minters: { minters, padding },
+      };
+      return { handleMsg };
+    },
+
+    burn({ padding }: Context, amount: string) {
+      const handleMsg = {
+        burn: { amount, padding },
+      };
+      return { handleMsg };
+    },
+
+    burnFrom({ padding }: Context, owner: string, amount: string) {
+      const handleMsg = {
+        burn_from: { owner, amount, padding },
+      };
+      return { handleMsg };
+    },
+
+    //Native
     deposit({ padding }: Context) {
       const handleMsg = {
         deposit: { padding },
