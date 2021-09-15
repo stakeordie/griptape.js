7cfd7bd63863845b0415708c161824df15f62ee2
jsandovalg <jorge@stakeordie.com>
2021-09-15 16:13:01 -0700

    feat: Add rest of SNIP-20 contract definition into Snip20Contract in types.ts file (#28)

--- src/contracts/types.ts
+++ src/contracts/types.ts
@@ -33,12 +33,39 @@ export interface Snip20Contract extends BaseContract {
   getBalance(): ContractRequest;
   getTokenInfo(): ContractRequest;
   getTransferHistory(page_size: number, page?: number): ContractRequest;
+  getMinters(): ContractRequest;
+  getAllowance(owner: string, spender: string, key: string): ContractRequest;
   getExchangeRate(): ContractRequest;
   transfer(recipient: string, amount: string): ContractRequest;
   send(recipient: string, amount: string, msg?: string): ContractRequest;
   registerReceived(code_hash: string): ContractRequest;
   createViewingKey(entropy: string): ContractRequest;
   setViewingKey(key: string): ContractRequest;
+  increaseAllowances(
+    spender: string,
+    amount: string,
+    expiration?: number
+  ): ContractRequest;
+  decreaseAllowance(
+    spender: string,
+    amount: string,
+    expiration?: number
+  ): ContractRequest;
+  transferFrom(
+    owner: string,
+    recipient: string,
+    amount: string
+  ): ContractRequest;
+  sendFrom(
+    owner: string,
+    recipient: string,
+    amount: string,
+    msg?: string
+  ): ContractRequest;
+  mint(recipient: string, amount: string): ContractRequest;
+  setMinters(minters: string[]): ContractRequest;
+  burn(amount: string): ContractRequest;
+  burnFrom(owner: string, amount: string): ContractRequest;
   deposit(): ContractRequest;
   redeem(amount: string, denom?: string): ContractRequest;
 }
