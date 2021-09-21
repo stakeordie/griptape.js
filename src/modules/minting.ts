d10c83701389b6aa37e7bdde6a3bc12ab9517a29
David SoD <david@stakeordie.com>
2021-09-21 11:40:26 -0700

    feat: Add Mint module #54

--- src/modules/minting.ts
+++ src/modules/minting.ts
@@ -1,19 +1,62 @@
-import axios, { AxiosInstance } from 'axios';
 import { getConfig } from '../bootstrap';
 import BlockchainModule from './base';
 
+/**
+ * @member {string} height indicates the current block in the chain
+ */
+export interface MintResponse {
+  /** indicates the current block in the chain */
+  height: string;
+}
+
+/**
+ * @member {string} height indicates the current block in the chain
+ * @member { ParametersResult } result is an object with mintable parameters
+ */
+export interface ParametersResponse extends MintResponse {
+  /**is an object with mintable parameters */
+  result: ParametersResult;
+}
+
+export interface ParametersResult {
+  mint_denom: string;
+  inflation_rate_change: string;
+  inflation_max: string;
+  inflation_min: string;
+  goal_bonded: string;
+  blocks_per_year: string;
+}
+
+/**
+ * @member {string} height indicates the current block in the chain
+ * @member {string} result is a string representation of the result
+ */
+export interface RequestResponse extends MintResponse {
+  /** is a string representation of the result  */
+  result: string;
+}
+
 export class MintingModule extends BlockchainModule {
-  async getParameters(): Promise<object> {
+  /**
+   * @returns ParametersResponse object with 'height' contains current block, 'result' contains all data
+   */
+  async getParameters(): Promise<ParametersResponse> {
     const res = await this.client.get('/minting/parameters');
     return res.data;
   }
 
-  async getInflation(): Promise<object> {
+  /**
+   * @returns RequestResponse object with [height] contains current block, 'result' contains inflation as string
+   */
+  async getInflation(): Promise<RequestResponse> {
     const res = await this.client.get('/minting/inflation');
     return res.data;
   }
 
-  async getAnualProvisions(): Promise<object> {
+  /**
+   * @returns RequestResponse object with 'height' contains current block, 'result' contains anual provisions as string
+   */
+  async getAnualProvisions(): Promise<RequestResponse> {
     const res = await this.client.get('/minting/annual-provisions');
     return res.data;
   }
