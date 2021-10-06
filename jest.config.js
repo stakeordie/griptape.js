b57493eac2f9ee052f5e2d9f6f1cdc706c45b866
luis-stakeordie <luis@stakeordie.com>
2021-10-06 10:49:47 -0700

    chore: PoC for testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+// Sync object
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+};
+
+module.exports = config;
---
+++ src/utils.test.ts
@@ -0,0 +1,18 @@
+import { coinConvert } from './utils';
+
+describe('coinConvert', () => {
+  test('Should convert from human to machine', () => {
+    const result = coinConvert(120, 6, 'machine');
+    expect(result).toBe('120000000');
+  });
+
+  test('Should convert from machine to human', () => {
+    const result = coinConvert(120000000, 6, 'human');
+    expect(result).toBe('120');
+  });
+
+  test('Should fixed the number to an specific number of decimals', () => {
+    const result = coinConvert(120345233, 6, 'human', 2);
+    expect(result).toBe('120.35');
+  });
+});
