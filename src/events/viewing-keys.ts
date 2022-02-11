ec0ac4a1029104e3ed68157010f44bfbd2af0601
Luis Espinoza <luis@stakeordie.com>
2022-02-10 17:17:52 -0800

    style: Format code

--- src/events/viewing-keys.ts
+++ src/events/viewing-keys.ts
@@ -1,14 +1,23 @@
-import { subscribeEvent, EventCallback, CleanListenerCallback, unsubscribeEventCallback } from './index';
+import {
+  subscribeEvent,
+  EventCallback,
+  CleanListenerCallback,
+  unsubscribeEventCallback,
+} from './index';
 
 /**
  * This event gets emitted when a viewing key is created.
  */
-export function onViewingKeyCreated(callback: EventCallback): CleanListenerCallback {
+export function onViewingKeyCreated(
+  callback: EventCallback
+): CleanListenerCallback {
   subscribeEvent('viewing-key-created', callback);
   return unsubscribeEventCallback('viewing-key-created', callback);
 }
 
-export function onViewingKeyReady(callback: EventCallback): CleanListenerCallback {
+export function onViewingKeyReady(
+  callback: EventCallback
+): CleanListenerCallback {
   subscribeEvent('viewing-key-ready', callback);
   return unsubscribeEventCallback('viewing-key-ready', callback);
 }
