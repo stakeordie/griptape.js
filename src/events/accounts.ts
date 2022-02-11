527f93d2446124f072a503ad4f1c3a69966dd1ce
Luis Espinoza <79593471+luis-stakeordie@users.noreply.github.com>
2022-02-10 16:15:42 -0800

    Revert "feat: Rename Callback type on src/events/index.ts"

--- src/events/accounts.ts
+++ src/events/accounts.ts
@@ -1,9 +1,9 @@
-import { subscribeEvent, EventCallback, unsubscribeEventCallback, CleanListenerCallback } from './index';
+import { subscribeEvent, Callback, unsubscribeEventCallback } from './index';
 
 /**
  * `account` event gets triggered when the Account Provider has an account ready.
  */
-export function onAccountAvailable(callback: EventCallback): CleanListenerCallback {
+export function onAccountAvailable(callback: Callback): Callback {
   subscribeEvent('account-available', callback);
   return unsubscribeEventCallback('account-available', callback);
 }
@@ -12,17 +12,17 @@ export function onAccountAvailable(callback: EventCallback): CleanListenerCallba
  * `account-change` events gets triggered when the account change on the Account
  * Provider.
  */
-export function onAccountChange(callback: EventCallback): CleanListenerCallback {
+export function onAccountChange(callback: Callback): Callback {
   subscribeEvent('account-change', callback);
   return unsubscribeEventCallback('account-change', callback);
 }
 
-export function onAccountNotAvailable(callback: EventCallback): CleanListenerCallback {
+export function onAccountNotAvailable(callback: Callback): Callback {
   subscribeEvent('account-not-available', callback);
   return unsubscribeEventCallback('account-not-available', callback);
 }
 
-export function onAccountDisconnect(callback: EventCallback): CleanListenerCallback {
+export function onAccountDisconnect(callback: Callback): Callback {
   subscribeEvent('shutdown', callback);
   return unsubscribeEventCallback('shutdown', callback);
 }
