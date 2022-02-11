711a5868affbff26d36c286412ef421116592ef9
VPringle <valeria.zuniga>
2022-02-10 16:05:07 -0800

    feat: event doesn't return CleanListenerCallback

--- src/events/viewing-keys.ts
+++ src/events/viewing-keys.ts
@@ -1,12 +1,14 @@
-import { subscribeEvent, EventCallback } from './index';
+import { subscribeEvent, EventCallback, CleanListenerCallback, unsubscribeEventCallback } from './index';
 
 /**
  * This event gets emitted when a viewing key is created.
  */
-export function onViewingKeyCreated(callback: EventCallback) {
+export function onViewingKeyCreated(callback: EventCallback): CleanListenerCallback {
   subscribeEvent('viewing-key-created', callback);
+  return unsubscribeEventCallback('viewing-key-created', callback);
 }
 
-export function onViewingKeyReady(callback: EventCallback) {
+export function onViewingKeyReady(callback: EventCallback): CleanListenerCallback {
   subscribeEvent('viewing-key-ready', callback);
+  return unsubscribeEventCallback('viewing-key-ready', callback);
 }
